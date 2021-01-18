3399fc2d7922ecd6e5943b94fa8ff9998d04c496
Ian V. Tatiana <ian.tatiana.v@gmail.com>
2021-01-18 13:21:08 +1000

    add assets menu & object tracking

---
+++ Assets/Scripts/MyImageTargetController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyImageTargetController : MonoBehaviour
+{
+    public Canvas myCanvas;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!gameObject.CompareTag("spaceshipTarget"))
+            return;
+
+        if (other.gameObject.CompareTag("characterTarget"))
+        {
+            myCanvas.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!gameObject.CompareTag("spaceshipTarget"))
+            return;
+
+        if (other.gameObject.CompareTag("characterTarget"))
+        {
+            myCanvas.gameObject.SetActive(false);
+        }
+    }
+}
